85a87485eeadee0f57f8faf39f8ac40ae9c64524
Takashi Makino <t-makino@atware.co.jp>
2024-01-15 06:43:41 +0900

    Add ref to video

--- components/organisms/hero.tsx
+++ components/organisms/hero.tsx
@@ -1,11 +1,22 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
+
 const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  }, [videoRef]);
+
   return (
     <div>
       <div className="relative w-full">
         <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50" />
         <video
+          ref={videoRef}
           src="/hero.mp4"
           autoPlay
           muted
