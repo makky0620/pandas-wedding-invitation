c1af8367844aebb0f0983178461bde498ec8da63
Takashi Makino <t-makino@atware.co.jp>
2024-01-20 16:45:11 +0900

    Add react-player and use it

--- components/organisms/hero.tsx
+++ components/organisms/hero.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useEffect, useRef } from 'react';
+import ReactPlayer from 'react-player';
 
 const Hero = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -15,18 +16,14 @@ const Hero = () => {
     <div>
       <div className="relative w-full">
         <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50" />
-        <video
-          ref={videoRef}
-          src="/hero.mp4"
-          height="750"
-          autoPlay
-          playsInline
+        <ReactPlayer
+          url="hero.mp4"
+          playing
           muted
           loop
-          controls
-        >
-          <source src="sample.mp4" type="video/mp4" />
-        </video>
+          width="100%"
+          height="100%"
+        />
         <div className="absolute bottom-[10%] left-4 text-white">
           <div className="text-5xl">HELLO!</div>
           <div>welcome to our wedding party</div>
